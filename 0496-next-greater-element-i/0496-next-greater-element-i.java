cce9b63b94afa0d683dd18b855e2ccc9b7fa441f
Sachin Kumar <sachinofficial7310@gmail.com>
2026-08-29 15:09:56 +0530

    Time: 5 ms (14.66%), Space: 44.5 MB (95.01%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,29 +4,28 @@ class Solution {
         // code here
       
     int res[]=new int[nums1.length];
-   boolean found =false;
     for(int i=0;i<nums1.length;i++){
-         found =false;
+      boolean  found =false;
         for(int j=0;j<nums2.length;j++){
              if(nums1[i]==nums2[j])
             {
                 found =true;
-                    for(int k=j+1;k<nums2.length;k++){
-                if (found && nums2[k]>nums1[i]){
-                    res[i]=nums2[k];
+                
+            }
+                if (found && nums2[j]>nums1[i]){
+                    res[i]=nums2[j];
                     break;
                 }
             }
 
+            
             if(res[i]==0){
                 res[i]=-1;
-            }
             }
         
             
  
         }
-    }
     return res;
 
     }
